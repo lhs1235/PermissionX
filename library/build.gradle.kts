a74e60a43588bc431c6288a67eb3e600722bf78a
liuhaosheng <liuhaosheng@xiaomi.com>
2024-08-16 14:08:12 +0800

    PermissionX2.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.permissionx.app"
+    namespace = "com.permissionx.lhs1235"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.permissionx.app"
         minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -37,11 +34,9 @@ android {
 
 dependencies {
 
-    implementation(project(":library"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
